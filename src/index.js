5f042a5468d19502bdd70235f1e3a4040f2dcbc0
Yuri Gor <YuriGor@users.noreply.github.com>
2021-04-19 12:56:43 +0200

    fixed comments

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 // import { mapValuesDeep } from 'deepdash/standalone'; // works in v5.3.7
 import { mapValuesDeep } from 'deepdash-es/standalone'; // works in v5.3.7
 // import mapValuesDeep from 'deepdash/mapValuesDeep'; // works in v5.3.7 - standalone method same as above
-// import mapValuesDeep from 'deepdash-es/mapValuesDeep'; // doesn't work by design, use import { mapValuesDeep } from 'deepdash-es/standalone'
+// import mapValuesDeep from 'deepdash-es/mapValuesDeep'; // doesn't work yet (will be back in v5.3.7), use import { mapValuesDeep } from 'deepdash-es/standalone'
 
 function component() {
   const element = document.createElement('div');
