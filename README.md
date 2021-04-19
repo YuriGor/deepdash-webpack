cd662b37cbb96db67d12af3ace141e4fcf1a8a4c
Yuri Gor <yuri.horus@gmail.com>
2021-04-19 12:24:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+`clone https://github.com/YuriGor/deepdash-webpack.git`
+`cd deepdash-webpack`
+`npm install`
+`npm start`
+http://localhost:5000/
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+// import standalone from 'deepdash/standalone'; // doesn't work by design, no default export, import {methods} explicitly
+// import standalone from 'deepdash-es/standalone'; // doesn't work by design, no default export, import {methods} explicitly
+// import { mapValuesDeep } from 'deepdash'; // doesn't work by design, deepdash is a function to mixin into Lodash
+// import { mapValuesDeep } from 'deepdash-es'; // doesn't work by design, deepdash is a function to mixin into Lodash
+// import { mapValuesDeep } from 'deepdash/standalone'; // works in v5.3.7
+import { mapValuesDeep } from 'deepdash-es/standalone'; // works in v5.3.7
+// import mapValuesDeep from 'deepdash/mapValuesDeep'; // works in v5.3.7 - standalone method same as above
+// import mapValuesDeep from 'deepdash-es/mapValuesDeep'; // doesn't work by design, use import { mapValuesDeep } from 'deepdash-es/standalone'
+
+function component() {
+  const element = document.createElement('div');
+  const data = { a: 'a', b: 'b' };
+
+  element.innerHTML = _.join(
+    [
+      'Hello',
+      'webpack',
+      JSON.stringify(
+        mapValuesDeep(data, (v) => v.toUpperCase(), { leavesOnly: true })
+      ),
+    ],
+    ' '
+  );
+
+  return element;
+}
+
+document.body.appendChild(component());
